ac6950b3ad5c1d36de51c63a8327f616e5813867
lucasnascimento95 <lucas.nascimento@leanonsystems.com>
2026-05-22 13:34:06 -0300

    scaffold project structure and configure cypress

---
+++ cypress.config.js
@@ -0,0 +1,10 @@
+const { defineConfig } = require('cypress')
+
+module.exports = defineConfig({
+  e2e: {
+    baseUrl: 'https://automationintesting.online',
+    specPattern: 'cypress/e2e/**/*.cy.js',
+    supportFile: 'cypress/support/e2e.js',
+    setupNodeEvents(on, config) {},
+  },
+})
