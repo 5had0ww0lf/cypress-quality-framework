23d3ce9925bae8ff3d4e7af251de526679e2acab
lucasnascimento95 <lucas.nascimento@leanonsystems.com>
2026-05-24 20:15:30 -0300

    add booking page object, utils and e2e tests

---
+++ cypress/e2e/ui/booking.cy.js
@@ -0,0 +1,55 @@
+import BookingPage from '../../support/pages/BookingPage';
+import { generateRandomDates } from '../../support/utils/bookingUtils';
+
+describe('Book Room', () => {
+  
+  let userData
+  
+  beforeEach(() => {
+    cy.fixture('booking').then((data) => {
+      userData = data
+    })
+    cy.visit('/')
+  });
+
+  it('should complete a room booking successfully', () => {
+    const {checkIn, checkOut} = generateRandomDates()
+    const {firstName, lastName, email, phone} = userData.user
+
+    BookingPage.checkAvailability(checkIn, checkOut)
+    BookingPage.selectRoom()
+    BookingPage.initiateReservation()
+    BookingPage.guestForm(firstName, lastName, email, phone)
+    BookingPage.finishReservationButton()
+
+    BookingPage.validateBooking()
+      .should('contain', 'Confirmed')
+  });
+
+  it('should show error when required fields are missing', () => {
+    const {checkIn, checkOut} = generateRandomDates()
+
+    BookingPage.checkAvailability(checkIn, checkOut)
+    BookingPage.selectRoom()
+    BookingPage.initiateReservation()
+    BookingPage.finishReservationButton()
+
+    cy.getErrorMessage()
+      .should('be.visible')
+  });
+  
+  it('should show error when fields contain invalid formats', () => {
+    const {checkIn, checkOut} = generateRandomDates()
+    const {firstName, lastName, email, phone} = userData.invalidUser
+
+    BookingPage.checkAvailability(checkIn, checkOut)
+    BookingPage.selectRoom()
+    BookingPage.initiateReservation()
+    BookingPage.guestForm(firstName, lastName, email, phone)
+    BookingPage.finishReservationButton()
+
+    cy.getErrorMessage()
+      .should('be.visible')    
+  });
+  
+})
---
+++ cypress/support/pages/BookingPage.js
@@ -0,0 +1,45 @@
+class BookingPage {
+
+  checkAvailability(checkIn, checkOut){
+    cy.get('.dateWrapper')
+      .first()
+      .find('input')
+      .clear()
+      .type(checkIn)
+    cy.get('.dateWrapper')
+      .last()
+      .find('input')
+      .clear()
+      .type(checkOut)
+
+    cy.contains('button', 'Check Availability').click()
+  }
+
+  selectRoom(){
+    cy.get('a[href*="/reservation/2"]').click()
+  }
+
+  initiateReservation(){
+    cy.get('#doReservation').click()
+  }
+
+  guestForm(firstName, lastName, email, phone){
+    cy.get('input[name="firstname"]').type(firstName)
+    cy.get('input[name="lastname"]').type(lastName)
+    cy.get('input[name="email"]').type(email)
+    cy.get('input[name="phone"]').type(phone)
+  }
+  
+  finishReservationButton(){
+    cy.contains('button', 'Reserve Now')
+      .click()
+  }
+
+validateBooking() {
+  return cy.get('div.card-body > h2')
+}
+
+
+}
+
+export default new BookingPage()
